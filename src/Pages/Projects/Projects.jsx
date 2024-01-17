db97dec1e791f5f048c792995521e5932cf2b723
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2024-01-17 04:44:44 +0400

    changes

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './Projects.css'
+import data from '../../../public/json/data.json'
+import { FaRegEye } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { Link } from 'react-router-dom';
 
 function Project() {
   return (
-    <div>
-      <h1>Project</h1>
+    <div className='container'>
+      <div className='projects-container'>
+        <div className='projects-header'>
+          <h2>My <span style={{ color: '#1bd5e1' }}>Works</span></h2>
+          <h3>Here are a few projects I`ve worked</h3>
+        </div>
+        <div className='projects-items'>
+          {data[3].projects.map((project, index) => (
+            <div className='projects-item' key={index}>
+              <div className='projects-item-img'>
+                <img src={project.img} alt={project.name} />
+              </div>
+              <div className='projects-item-texts'>
+                <div className='projects-item-header'>
+                  <h3>{project.name}</h3>
+                </div>
+                <div className='projects-item-explanation'>
+                  <p>{project.explanation}</p>
+                </div>
+                <div className='projects-item-buttons'>
+                  <Link to={project['github-link']} target='_blank'><button><FiGithub />Github</button></Link>
+                  <Link to={project['preview-link']} target='_blank'><button><FaRegEye />Preview</button></Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
