da2083d8259c5d9488ffae84dd2473c5308c7c17
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2024-01-21 14:56:26 +0400

    changes

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,44 +1,60 @@
-import React from 'react'
-import './Footer.css'
-import { FaLinkedinIn , FaWhatsapp} from "react-icons/fa";
-import { FiGithub } from "react-icons/fi";
-import { SiGmail } from "react-icons/si";
+import React from 'react';
+import './Footer.css';
+import { FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
+import { SiGmail } from 'react-icons/si';
 import { FaInstagram } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-bg'>
-        <div className='container'>
-            <div className='footer-columns'>
-                <div className='footer-column'>
-                    <h5>Designed and Developed by <b>Ayaz Damirov</b></h5>
+      <div className='container'>
+        <div className='footer-columns'>
+          <div className='footer-column'>
+            <h5>
+              Designed and Developed by{' '}
+              <Link style={{ color: 'white' }} to={'https://www.linkedin.com/in/ayaz-damirov/'} target='_blank'>
+                <b>Ayaz Damirov</b>
+              </Link>
+            </h5>
+          </div>
+          <div className='footer-column'>
+            <h5>Copyright © 2024</h5>
+          </div>
+          <div className='footer-column'>
+            <div className='footer-social-media'>
+              <Link style={{ color: 'white' }} to={'https://www.linkedin.com/in/ayaz-damirov/'} target='_blank'>
+                <div>
+                  <FaLinkedinIn />
                 </div>
-                <div className='footer-column'>
-                    <h5>Copyright © 2024</h5>
+              </Link>
+              <Link style={{ color: 'white' }} to={'https://github.com/DemirovAYAZ'} target='_blank'>
+                <div>
+                  <FiGithub />
                 </div>
-                <div className='footer-column'>
-                    <div className='footer-social-media'>
-                        <div>
-                            <FaLinkedinIn />
-                        </div>
-                        <div>
-                            <FiGithub />
-                        </div>
-                        <div>
-                            <FaWhatsapp />
-                        </div>
-                        <div>
-                            <FaInstagram />
-                        </div>
-                        <div>
-                            <SiGmail/>
-                        </div>
-                    </div>
+              </Link>
+              <Link style={{ color: 'white' }} to={'tel:+994559014435'} target='_blank'>
+                <div>
+                  <FaWhatsapp />
                 </div>
+              </Link>
+              <Link style={{ color: 'white' }} to={'https://www.instagram.com/ayazdamirov_/'} target='_blank'>
+                <div>
+                  <FaInstagram />
+                </div>
+              </Link>
+              <Link style={{ color: 'white' }} to={'mailto:ayazdemirov123@gmail.com'} target='_blank'>
+                <div>
+                  <SiGmail />
+                </div>
+              </Link>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -18,21 +18,31 @@ function Contact() {
           <h2 className='contact-texts-h2'>Contact Me</h2>
           <p className='contact-texts-p'>I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.</p>
           <div className='social-medias'>
-            <div>
-              <FaLinkedinIn />
-            </div>
-            <div>
-              <FiGithub />
-            </div>
-            <div>
-              <FaWhatsapp />
-            </div>
-            <div>
-              <FaInstagram />
-            </div>
-            <div>
-              <SiGmail/>
-            </div>
+            <Link style={{ color: 'white' }} to={'https://www.linkedin.com/in/ayaz-damirov/'} target='_blank'>
+              <div>
+                <FaLinkedinIn />
+              </div>
+            </Link>
+            <Link style={{ color: 'white' }} to={'https://github.com/DemirovAYAZ'} target='_blank'>
+              <div>
+                <FiGithub />
+              </div>
+            </Link>
+            <Link style={{ color: 'white' }} to={'tel:+994559014435'} target='_blank'>
+              <div>
+                <FaWhatsapp />
+              </div>
+            </Link>
+            <Link style={{ color: 'white' }} to={'https://www.instagram.com/ayazdamirov_/'} target='_blank'>
+              <div>
+                <FaInstagram />
+              </div>
+            </Link>
+            <Link style={{ color: 'white' }} to={'mailto:ayazdemirov123@gmail.com'} target='_blank'>
+              <div>
+                <SiGmail />
+              </div>
+            </Link>
           </div>
           <Link to='/resume'><button className='contact-button'>See My Resume</button></Link>
         </div>
